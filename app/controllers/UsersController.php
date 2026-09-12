60694be07f12eb251ab6e170bbd72ce0e420c5d1
Maverick Telmo Gutierrez <maverickgutierrez3@gmail.com>
2026-09-12 21:12:22 +0800

    Activity 5: CRUD with Authentication

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -5,15 +5,10 @@ class UsersController extends Controller {
 
     public function __construct() {
         parent::__construct();
-        // I-load ang Users_model
         $this->call->model('Users_model');
     }
-
     public function index() {
-        // Kunin ang records mula sa model
         $data['users'] = $this->Users_model->get_all_users();
-        
-        // I-pass ang data sa view
         $this->call->view('users_view', $data);
     }
 }
